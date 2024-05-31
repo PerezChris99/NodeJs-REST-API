4168b694e55cf3059009792f278538fb4077e919
unknown <kweeziperez712@gmail.com>
2024-05-31 13:35:42 +0300

    can write data to json file

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const { getProducts, getProduct, createProduct } = require('./controllers/produc
 const server = http.createServer((req, res) => {
     if(req.url === '/api/products' && req.method === 'GET'){
         getProducts(req, res)
+
     } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET')
     {
        const id = req.url.split('/')[3]
         getProduct(req, res, id) 
+
+    } else if(req.url === '/api/products' && req.method === 'POST'){
+        createProduct(req, res)
+        
     } else {
         res.writeHead(404, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify({ message: 'Route Not Found'}))
